93515ce1c71f2d5115fcb5f830e3df8b77c7f883
王路路 <wllane@163.com>
2018-08-25 11:12:47 +0800

    create react-demo with webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+﻿var path =require('path');
+
+module.exports = {
+    
+	//入口文件
+        
+	entry:'./app/index.js',
+	    
+	//出口文件
+	        
+	output:{
+		        
+		filename:'index.js',
+			        
+		path:path.resolve(__dirname,'dist')
+				    
+	}
+				    	
+}
